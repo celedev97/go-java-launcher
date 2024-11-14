celedev97/go-java-launcher

commit 912ed435e8dbc0a8d4665af0d5a01b6b831be7c9

feat: added command line args to jar launch

javahelper/helper_common.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 )
 
-//DownloadJava downloads a Java installer, and return a string containing the installer location,
-//if something goes wrong it returns an error
+// DownloadJava downloads a Java installer, and return a string containing the installer location,
+// if something goes wrong it returns an error
 func DownloadJava(javaVersion int) (string, error) {
 	version := strconv.Itoa(javaVersion)
 	filename := "adoptopenjdk.jre." + version + "." + ARCHITECTURE + EXTENSION
@@ -30,7 +30,7 @@ func DownloadJava(javaVersion int) (string, error) {
 	return filename, nil
 }
 
-//WhereJava run where/which java to search for java installations
+// WhereJava run where/which java to search for java installations
 func whereJava() ([]string, error) {
 	// Executing "where java"
 	whereJavaOutput, err := Command(WHERE, "java").CombinedOutput()
@@ -71,12 +71,17 @@ func filterJavas(version int, javas []string) []string {
 	return output
 }
 
-//RunJava is just a shortcut for javaw -jar filename
-func RunJava(java string, filename string) error {
+// RunJava is just a shortcut for javaw -jar filename
+func RunJava(java string, filename string, args []string) error {
 	absoluteFileName, err := filepath.Abs(filename)
 	if err != nil {
 		return err
 	}
-	println(java, " -jar ", absoluteFileName)
-	return Command(java, "-jar", absoluteFileName).Start()
+
+	//put filename as the first argument, -jar as the second and the rest of the arguments after
+	args = append([]string{"-jar"}, args...)
+	args = append([]string{absoluteFileName}, args...)
+
+	println(java, strings.Join(args, " "))
+	return Command(java, args...).Run()
 }

.gitignore
@@ -1,12 +1,7 @@
 # FROM https://github.com/github/gitignore/blob/master/Global/VisualStudioCode.gitignore
 # Visual studio
 
-.vscode/*
-!.vscode/settings.json
-!.vscode/tasks.json
-!.vscode/launch.json
-!.vscode/extensions.json
-*.code-workspace
+.vscode
 
 # Local History for Visual Studio Code
 .history/

go.mod
@@ -1,7 +1,8 @@
-module github.com/fcdev/go-java-launcher
+module github.com/celedev97/go-java-launcher
 
 go 1.15
 
 require (
 	fyne.io/fyne/v2 v2.4.1
+	github.com/akavel/rsrc v0.10.2 // indirect
 )

go.sum
@@ -47,6 +47,7 @@ fyne.io/systray v1.10.1-0.20230722100817-88df1e0ffa9a/go.mod h1:oM2AQqGJ1AMo4nNq
 github.com/BurntSushi/toml v0.3.1/go.mod h1:xHWCNGjB5oqiDr8zfno3MHue2Ht5sIBksp03qcyfWMU=
 github.com/BurntSushi/toml v1.3.2/go.mod h1:CxXYINrC8qIiEnFrOxCa7Jy5BFHlXnUU2pbicEuybxQ=
 github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802/go.mod h1:IVnqGOEym/WlBOVXweHU+Q+/VP0lqqI8lqeDx9IjBqo=
+github.com/akavel/rsrc v0.10.2 h1:Zxm8V5eI1hW4gGaYsJQUhxpjkENuG91ki8B4zCrvEsw=
 github.com/akavel/rsrc v0.10.2/go.mod h1:uLoCtb9J+EyAqh+26kdrTgmzRBFPGOolLWKpdxkKq+c=
 github.com/antihax/optional v1.0.0/go.mod h1:uupD/76wgC+ih3iEmQUL+0Ugr19nfwCT1kdvxnR2qWY=
 github.com/armon/circbuf v0.0.0-20150827004946-bbbad097214e/go.mod h1:3U/XgcO3hCbHZ8TKRvWD2dDTCfh9M9ya+I9JpbB7O8o=

main.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 
-	helper "github.com/fcdev/go-java-launcher/javahelper"
+	helper "github.com/celedev97/go-java-launcher/javahelper"
 )
 
 func criticalError(err error) {
@@ -17,7 +17,7 @@ func criticalError(err error) {
 const CONFIG string = "go-java.json"
 
 func main() {
-	javaVersion := 11
+	javaVersion := 21
 	launch := "./app.jar"
 
 	//reading the configuration file
@@ -34,6 +34,9 @@ func main() {
 			//reading javaVersion
 			if val, exists := settings["javaVersion"]; exists {
 				javaVersion, err = strconv.Atoi(val.(json.Number).String())
+				if err != nil {
+					javaVersion = 21
+				}
 			}
 			if val, exists := settings["launch"]; exists {
 				launch = val.(string)
@@ -62,7 +65,10 @@ func main() {
 
 	println("FOUND JAVA: " + java)
 
-	err = helper.RunJava(java, launch)
+	//get arguments
+	args := os.Args[1:]
+
+	err = helper.RunJava(java, launch, args)
 	if err != nil {
 		criticalError(err)
 	}

commit 3b5dedc075eb1689d543196c429401bd3dafa208

fix: fixed a bug caused by 912ed43

javahelper/helper_common.go
@@ -79,8 +79,8 @@ func RunJava(java string, filename string, args []string) error {
 	}
 
 	//put filename as the first argument, -jar as the second and the rest of the arguments after
-	args = append([]string{"-jar"}, args...)
-	args = append([]string{absoluteFileName}, args...)
+	firstArgs := []string{"-jar", absoluteFileName}
+	args = append(firstArgs, args...)
 
 	println(java, strings.Join(args, " "))
 	return Command(java, args...).Run()